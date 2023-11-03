44877e2e6cf6f0f62f9ed40659200daa84a1f062
Lukas <148761635+Lucas-Sawicki@users.noreply.github.com>
2023-11-03 11:09:03 +0100

    Revert "Feature"

--- src/main/java/pl/zajavka/case1/Animal.java
+++ src/main/java/pl/zajavka/case1/Animal.java
@@ -1,10 +1,4 @@
 package pl.zajavka.case1;
 
-public abstract class Animal {
-
-    private String name;
-
-    protected String whatIsYourName() {
-        return "I don't have any";
-    }
+public class Animal {
 }
